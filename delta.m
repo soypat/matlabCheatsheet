c1940396ed44d0c17724dcac692033582ad9712c
soypat <graded.sp@gmail.com>
2018-02-20 20:11:10 -0300

    finalize organization of names. new alphabet

--- delta.m
+++ delta.m
@@ -1,23 +1,14 @@
-%------------------------------------------------------------------------%
-load datos.mat
-fig=figure;   %Genero el diagrama y lo guardo como fig para editar despues
-blk=[0 0 0];  %Guardo el color negro en blk
-set(fig,'defaultAxesColorOrder',[blk; blk]) %cambio color de ejes
-%------------------------------------------------------------------------%
-semilogx(rev,fev1,'b-')
-hold on                 %Hace falta tener un hold on
-semilogx(rev,fev2,'r-')
-semilogx(rev,fev3,'c-') 
-semilogx(rev,fev4,'y-')
-yl=ylim;                %Guarda los limites del eje para tener referencia
-                        %cuando haya un nuevo eje y
-title('Mi Gran Grafico')
-xlabel('Valor Independiente')
-ylabel('Valor Dependiente')
-yyaxis right            %Crea un nuevo eje y queda como eje activo
-yticks('manual')        %Esto va permitir modificar los ticks del nuevo eje
-yticks([fev1(end),fev2(end),fev3(end),fev4(end)]) %Guarda posiciones de los ticks
-ylim(yl)                %Iguala limites del nuevo eje al del primer eje
-yticklabels({'Azul!' , 'Rojo!', 'Celeste!' ,'Amarillo!'})
-ylabel('Los Colores del Arco iris \heartsuit')
-grid on
+%-------------------------------------------------------------------------%
+syms x y %Para trabajar con la funcion latex de matlab hay que declarar los
+         %variables que se van a usar como simbolicos
+valoresZ=peaks(100); %Devuelve 100x100 valores de la funcion prueba de Matlab
+xval=1:100;
+yval=1:100;
+surf(xval,yval,valoresZ); %Grafico la superficie de peaks con un mapa 100x100
+z =  3*(1-x)^2*exp(-(x^2) - (y+1)^2) ... 
+   - 10*(x/5 - x^3 - y^5)*exp(-x^2-y^2) ... 
+   - 1/3*exp(-(x+1)^2 - y^2); %Funcion graficada (es peaks)
+title(['La ecuacion: $ ' latex(z) ' $'],'Interpreter','latex')
+%Pongo lo que se va hacer pasar por el compilador de latex entre los
+%corchetes
+
