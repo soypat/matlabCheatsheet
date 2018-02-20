e7c748954e84259217d70da499193f3980222147
acanalis <acanalis@itba.edu.ar>
2018-02-20 19:11:52 -0300

    Change alpha.m 2x2 subplot + increase visibility

--- alpha.m
+++ alpha.m
@@ -3,11 +3,11 @@ B=randi(11,20,1);
 C=randi([8,10],10,3);
 tufuncion=@(x)cos(3*x)*x;
 
-subplot(2,3,1),plot(A,':g')
+subplot(2,2,1),plot(A,'sg')
 grid on
-subplot(2,3,2),plot(B,'y*')
-subplot(2,3,3),fplot(tufuncion,[0,2*pi],'--k')
-subplot(2,3,4),plot(C)
+subplot(2,2,2),plot(B,'y*')
+subplot(2,2,3),fplot(tufuncion,[0,2*pi],'--k')
+subplot(2,2,4),plot(C)
 title('2-D Line Plot')
 xlabel('x')
 ylabel('cos(5x)')
